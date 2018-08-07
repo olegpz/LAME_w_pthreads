beff3d9f0c205cc6c13e5748227e4a38fc1748a7
Oleg Prosekov <olegp@axis.com>
2018-08-07 17:23:06 +0200

    MP3 enc
    Change-Id: I2f3fcca88d7d396461fde00349199db1f837ce16

--- main.cc
+++ main.cc
@@ -5,51 +5,39 @@
 #include <list>
 #include <string>
 #include <vector>
-#include <ctime>
-#include "dirent.h"	/* this is used to get cross-platform directory listings without Boost. */
-
-#include <stdio.h>
 #include <vector>
 #include <sstream>
+#include <time.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <pthread.h>
+#include <sys/sysinfo.h>
 #include "lame.h"
 #include "wave.h"
-#include "pthread.h"
-#ifndef WIN32
-#include "sys/sysinfo.h"
-#endif
 
-static pthread_mutex_t mutFilesFinished = PTHREAD_MUTEX_INITIALIZER;
-
-#ifdef WIN32
-#define PATHSEP "\\"
-#else
-#define PATHSEP "/"
-#endif
+static pthread_mutex_t mutex_finished = PTHREAD_MUTEX_INITIALIZER;
 
 using namespace std;
 
-/*
- * POSIX-conforming argument struct for worker routine 'lame_encoder'.
- */
 typedef struct {
-	vector<string> *pFilenames;
-	bool *pbFilesFinished;
-	int iNfiles;
-	int iThreadId;
-	int iProcessedFiles;
-} ENC_WRK_ARGS;
+	vector<string> *file_name;
+	bool *flag_enc;
+	int num_files;
+	int th_id;
+	int num_enc;
+} ENC_ARGS;
 
 /* Parse a directory <dirname> and return a list of
  * filenames within this directory. */
 vector<string> parse_directory(const char *dirname)
 {
     struct dirent *de;  // Pointer for directory entry
-    vector<string> dirEnt;
+    vector<string> dircontx;
     DIR *dr = opendir(dirname);
  
     if (dr == NULL) {
         printf("Could not open current directory\n");
-	    return dirEnt;
+	    return dircontx;
     }
  
     // http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html
@@ -58,144 +46,109 @@ vector<string> parse_directory(const char *dirname)
             int len = strlen(de->d_name);
             //printf("len = %d\n", len);
             if (len > 4)
-                if (!strcmp(de->d_name+len-3, "wav")) {
-                    dirEnt.push_back(string(dirname) + string(PATHSEP) + string(de->d_name));
+                if (!strcmp(de->d_name+len-4, ".wav")) {
+                    dircontx.push_back(string(dirname) + "/" + string(de->d_name).substr(0,len-4));
                     printf("%s\n", de->d_name);
                 }
     }
-    printf("--- Found %ld WAV file(s)\n\n", dirEnt.size());
+    printf("--- Found %ld WAV file(s)\n\n", dircontx.size());
  
     closedir(dr);
 
-	return dirEnt;
+	return dircontx;
 }
 
-/* encode_to_file
- *  Main encoding routine which reads input information from gfp and hdr as well as one or two PCM buffers,
- *  encodes it to MP3 and directly stores the MP3 data in the file given by filename.
- *  Calls lame_encode_buffer, lame_encode_flush, and lame_mp3_tags_fid internally for a complete conversion
- *  process.
- */
-int encode_to_file(lame_global_flags *gfp, const FMT_DATA *hdr, const short *leftPcm, const short *rightPcm,
-	const int iDataSize, const char *filename)
-{
-	int numSamples = iDataSize / hdr->wBlockAlign;
-
-	int mp3BufferSize = numSamples * 5 / 4 + 7200; // worst case estimate
-	unsigned char *mp3Buffer = new unsigned char[mp3BufferSize];
-
-	// call to lame_encode_buffer
-	int mp3size = lame_encode_buffer(gfp, (short*)leftPcm, (short*)rightPcm, numSamples, mp3Buffer, mp3BufferSize);
-	if (!(mp3size > 0)) {
-		delete[] mp3Buffer;
-		cerr << "No data was encoded by lame_encode_buffer. Return code: " << mp3size << endl;
-		return 1;
-	}
-
-	// write to file
-	FILE *out = fopen(filename, "wb+");
-	fwrite((void*)mp3Buffer, sizeof(unsigned char), mp3size, out);
-
-	// call to lame_encode_flush
-	int flushSize = lame_encode_flush(gfp, mp3Buffer, mp3BufferSize);
-
-	// write flushed buffers to file
-	fwrite((void*)mp3Buffer, sizeof(unsigned char), flushSize, out);
-
-	// call to lame_mp3_tags_fid (might be omitted)
-	lame_mp3_tags_fid(gfp, out);
-
-	fclose(out);
-	delete[] mp3Buffer;
-
-#ifdef __VERBOSE_
-	cout << "Wrote " << mp3size + flushSize << " bytes." << endl;
-#endif
-	return 0;
-}
-
-/* lame_encoder
- *  Main worker thread routine which is supplied with a list of filenames, a status array indicating which files
- *  are already worked upon, and some additional info via a ENC_WRK_ARGS struct.
- *  As long as there are still unprocessed filenames, this routine will fetch the next free filename, mark it as
- *  processed, and execute the complete conversion from reading .wav to writing .mp3.
- */
 void *lame_encoder(void* arg)
 {
 	int ret;
-	ENC_WRK_ARGS *args = (ENC_WRK_ARGS*)arg; // parse argument struct
+	ENC_ARGS *args = (ENC_ARGS*)arg;
 
 	while (true) {
-#ifdef __VERBOSE_
-		cout << "Checking for work\n";
-#endif
-		// determine which file to process next
-		bool bFoundWork = false;
-		int iFileIdx = -1;
+		bool wflag = false;
+		int file_id = -1;
 
-		pthread_mutex_lock(&mutFilesFinished);
-		for (int i = 0; i < args->iNfiles; i++) {
-			if (!args->pbFilesFinished[i]) {
-				args->pbFilesFinished[i] = true; // mark as being worked on
-				iFileIdx = i;
-				bFoundWork = true;
+		pthread_mutex_lock(&mutex_finished);
+
+		for (int i = 0; i < args->num_files; i++) {
+			if (!args->flag_enc[i]) {
+				args->flag_enc[i] = true;
+				file_id = i;
+				wflag = true;
 				break;
 			}
 		}
-		pthread_mutex_unlock(&mutFilesFinished);
 
-		if (!bFoundWork) {// done yet?
+		pthread_mutex_unlock(&mutex_finished);
+
+		if (!wflag)
 			return NULL; // break
-		}
-		string sMyFile = args->pFilenames->at(iFileIdx);
-		string sMyFileOut = sMyFile.substr(0, sMyFile.length() - 3) + "mp3";
 
-		// start working
+		string wav = args->file_name->at(file_id)+".wav";
+		string mp3 = args->file_name->at(file_id)+".mp3";
+        const char* wav_filename = wav.c_str();
+		const char* mp3_filename = mp3.c_str();
+
 		FMT_DATA *hdr = NULL;
-		short *leftPcm = NULL, *rightPcm = NULL;
+        short *left = NULL, *right = NULL;
 		// init encoding params
 		lame_global_flags *gfp = lame_init();
-		lame_set_brate(gfp, 192); // increase bitrate
-		lame_set_quality(gfp, 3); // increase quality level
-		lame_set_bWriteVbrTag(gfp, 0);
+		lame_set_quality(gfp, 5); // good quality level
 
-		// parse wave file
-#ifdef __VERBOSE_
-		printf("Parsing %s ...\n", sMyFile.c_str());
-#endif
-		int iDataSize = -1;
-		ret = read_wave(sMyFile.c_str(), hdr, leftPcm, rightPcm, iDataSize);
-		if (ret != 0) {
-			printf("Error in file %s. Skipping.\n", sMyFile.c_str());
-			continue; // see if there's more to do
+		// parse wav file
+		int data_sz = -1;
+		ret = read_wave(wav_filename, hdr, left, right, data_sz);
+		if (ret) {
+			fprintf(stderr, "Error in file %s. Skipping.\n\n", wav_filename);
+			continue;
 		}
 
 		lame_set_num_channels(gfp, hdr->wChannels);
-		lame_set_num_samples(gfp, iDataSize / hdr->wBlockAlign);
-		// check params
+		lame_set_num_samples(gfp, data_sz / hdr->wBlockAlign);
+
 		ret = lame_init_params(gfp);
 		if (ret != 0) {
-			cerr << "Invalid encoding parameters! Skipping file." << endl;
+			fprintf(stderr, "Invalid encoding parameters! Skipping file.\n\n");
 			continue;
 		}
 
-		// encode to mp3
-		ret = encode_to_file(gfp, hdr, leftPcm, rightPcm, iDataSize, sMyFileOut.c_str());
-		if (ret != 0) {
-			cerr << "Unable to encode mp3: " << sMyFileOut.c_str() << endl;
-			continue;
-		}
 
-		printf("[:%i][ok] .... %s\n", args->iThreadId, sMyFile.c_str());
-		++args->iProcessedFiles;
+        int numSamples = data_sz / hdr->wBlockAlign;
+        int mp3BufferSize = numSamples * 5 / 4 + 7200; // worst case estimate
+        unsigned char *mp3Buffer = (unsigned char*)malloc(mp3BufferSize);
+
+        // call to lame
+        int mp3size = lame_encode_buffer(gfp, (short*)left, (short*)right, numSamples, mp3Buffer, mp3BufferSize);
+
+        if (!(mp3size > 0)) {
+            free(mp3Buffer);
+            fprintf(stderr, "No data was encoded by lame_encode_buffer. Return code: %d\n", mp3size);
+            fprintf(stderr, "Unable to encode mp3: %s\n\n", mp3_filename);
+            continue;
+        }
+
+        // write to file
+        FILE *out = fopen(mp3_filename, "wb+");
+        fwrite((void*)mp3Buffer, sizeof(unsigned char), mp3size, out);
+        int flushSize = lame_encode_flush(gfp, mp3Buffer, mp3BufferSize);
+        fwrite((void*)mp3Buffer, sizeof(unsigned char), flushSize, out);
+
+        // might be omitted
+        lame_mp3_tags_fid(gfp, out);
+
+        fclose(out);
+        free(mp3Buffer);
+
+
+		printf("[Thread:%i -- %s]\n", args->th_id, mp3_filename);
+		++args->num_enc;
 
 		lame_close(gfp);
-		if (leftPcm != NULL) delete[] leftPcm;
-		if (rightPcm != NULL) delete[] rightPcm;
-		if (hdr != NULL) delete hdr;
+		if  (left != NULL) free(left);
+		if (right != NULL) free(right);
+		if  (hdr  != NULL) free(hdr);
 	}
 
-	pthread_exit((void*)0);
+	pthread_exit(NULL);
 }
 
 
@@ -221,46 +174,43 @@ int main(int argc, char **argv)
 
 	if (!nfiles) return 0;
 
-	// initialize pbFilesFinished array which contains true for all files which are currently already converted
-	bool pbFilesFinished[nfiles];
-	for (int i = 0; i < nfiles; i++) pbFilesFinished[i] = false;
+	// initialize flag_enc array which contains true for all files which are currently already converted
+	bool flag_enc[nfiles];
+	for (int i = 0; i < nfiles; i++) flag_enc[i] = false;
 
 
 	// initialize threads array and argument arrays
 	pthread_t *threads = new pthread_t[nprocs];
-	ENC_WRK_ARGS *threadArgs = (ENC_WRK_ARGS*)malloc(nprocs * sizeof(ENC_WRK_ARGS));
+	ENC_ARGS *threadArgs = (ENC_ARGS*)malloc(nprocs * sizeof(ENC_ARGS));
 	for (int i = 0; i < nprocs; i++) {
-		threadArgs[i].iNfiles = nfiles;
-		threadArgs[i].pFilenames = &wavfiles;
-		threadArgs[i].pbFilesFinished = pbFilesFinished;
-		threadArgs[i].iThreadId = i;
-		threadArgs[i].iProcessedFiles = 0;
+		threadArgs[i].num_files = nfiles;
+		threadArgs[i].file_name = &wavfiles;
+		threadArgs[i].flag_enc = flag_enc;
+		threadArgs[i].th_id = i;
+		threadArgs[i].num_enc = 0;
 	}
 
 	clock_t start_clk = clock();
 
-	// create worker threads
 	for (int i = 0; i < nprocs; i++) {
 		pthread_create(&threads[i], NULL, lame_encoder, (void*)&threadArgs[i]);
 	}
 
-	// synchronize / join threads
 	for (int i = 0; i < nprocs; i++) {
 		int ret = pthread_join(threads[i], NULL);
-		if (ret != 0) {
-			printf("\tpthread error occured!!!\n");
-		}
+		if (ret != 0) printf("\tpthread error occured!!!\n");
 	}
+    printf("\n");
 
 	clock_t end_clk = clock();
 
 	int iProcessedTotal = 0;
 	for (int i = 0; i < nprocs; i++) {
-		printf("Thread %d processed %d files.\n", i, threadArgs[i].iProcessedFiles);
-		iProcessedTotal += threadArgs[i].iProcessedFiles;
+		printf("Thread %d processed %d files.\n", i, threadArgs[i].num_enc);
+		iProcessedTotal += threadArgs[i].num_enc;
 	}
 
-	printf("Converted %d out of %d files in total in %g sec.\n", iProcessedTotal, nfiles, double(end_clk - start_clk) / CLOCKS_PER_SEC);
+	printf("\nEncoded %d out of %d files in total in %g sec.\n", iProcessedTotal, nfiles, double(end_clk - start_clk) / CLOCKS_PER_SEC);
 
 	free(threads);
 	free(threadArgs);
